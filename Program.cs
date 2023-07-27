90cfec7be210a5f5d5544d356f83322dcfae1c6f
Murillo Alves <tumusx1@gmail.com>
2023-07-26 22:32:22 -0300

    Finished basic content

--- Program.cs
+++ Program.cs
@@ -1,2 +1,45 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿void randomNumber()
+{
+    int randomNumber = new Random().Next(1, 100);
+    Console.WriteLine(randomNumber);
+    int inputNumberUser = 0;
+    do
+    {
+        inputNumberUser = int.Parse(Console.ReadLine()!);
+        if (inputNumberUser > randomNumber)
+        {
+            Console.WriteLine("Número digitado é maior");
+        }
+
+        if (inputNumberUser < randomNumber)
+        {
+            Console.WriteLine("Número digitado é menor!");
+        }
+
+        if (inputNumberUser == randomNumber)
+        {
+            Console.WriteLine("Parabéns!! Número é igual ao gerado randomicamente!");
+        }
+    } while (inputNumberUser != randomNumber);
+}
+randomNumber();
+
+void createList()
+{
+    List<String> listOfString = new List<String>();
+    listOfString.Add("Murillo is a happy man");
+    listOfString.Add("Murillo is a software developer");
+    Dictionary<String, List<int>> subMurillo = new Dictionary<String, List<int>>();
+    foreach (var s in listOfString)
+    {
+        Console.WriteLine(s);
+        subMurillo.Add(s, new List<int>(new Random().Next()));
+        foreach (var dicMurillo in subMurillo)
+        {
+            Console.WriteLine(dicMurillo.Value);
+        }
+    }
+
+}
+
+createList();
